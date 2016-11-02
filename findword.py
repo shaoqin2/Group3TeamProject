aa7584e3dd6abdd70d9fb49a07fcae454afb48bd
shaoqin2 <shaoqin2@illinois.edu>
2016-11-02 14:46:54 +0000

    post word fixed

--- findword.py
+++ findword.py
@@ -1,32 +1,4 @@
-'''import MySQLdb
-from connectdb import connection
-import json
 
-def fword(lon,lat,radius,ids):
-    #retrieve all datas in the vicinity of this point
-    #filter the data to take off all existing ids
-    #return a json string to init.py
-    try:
-        result = {}
-        c, conn = connection()
-        c.execute("SELECT * FROM wordlocation WHERE ((longitude-%s)*(longitude-%s)+(latitude-%s)*(latitude-%s))<%s",(lon,lon,lat,lat,radius))
-        words = []
-		for row in c.fetchall():
-			t = {}
-			if row[0] in ids:
-				continue
-			t["name"] = row[1]
-			t["definition"] = row[4]
-			t["id"] = row[0]
-			t["lat"] = row[3]
-			t["long"] = row[2]
-			words.append(t)
-		result["words"] = words
-		j_result = json.dumps(result)
-		return j_result
-	except Exception as e:
-		return str(e)
-'''
 import MySQLdb
 from connectdb import connection
 import json
@@ -51,3 +23,13 @@ def fword(lon,lat,radius,ids):
         return j
     except Exception as e:
         return str(e)
+
+def pword(lat,lng,word,definition):
+	try:
+		c, conn = connection()
+		c.execute("INSERT INTO wordlocation (word,longitude,latitude,definition,lang) VALUES(%s,%s,%s,%s,%s)",
+		(word,lat,lng,definition,'english'))
+		conn.commit()
+		return "success!"
+	except Exception as e:
+		return str(e)
